ccf6960e61f58201c4a4a0d9d28b6ed48642145a
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-02-21 15:53:27 +0200

    add and style header section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,110 @@
+<script setup>
+import logo from '../assets/logo/logo-dark.png'
+import hamburger from '../assets/icon-hamburger.svg'
+import close from '../assets/icon-close.svg'
+import { ref } from 'vue'
+
+let isOpen = ref(false)
+function handleClick() {
+    isOpen.value = !isOpen.value
+}
+</script>
+
 <template>
-    <div class="container">
-        <h1>Header</h1>
-        <p>This is Header</p>
-    </div>
+    <div className="navbar">
+            <div className="container flex items-center justify-between">
+                <router-link :to="{name:'Home'}">
+                        <img
+                            :src="logo"
+                            alt='Logo image and home page button'
+                            width=200px
+                            height=37px
+                        />
+                </router-link>
+                <div>
+                    <button @click="handleClick" className="mobile-menu">
+                        <img :src="`${isOpen ? close : hamburger}`"  alt='open menu'/>
+                        
+                    </button>
+                    <div :className="`navbar-links ${isOpen ? 'open' : 'close'}`">
+                        <router-link :to="{name:'Company'}">Our company</router-link>
+                        <router-link :to="{name:'Location'}">Location</router-link>
+                        <router-link :to="{name:'Contact'}">Contact</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- { isOpen && 
+            <div className="navbar-overlay">
+            </div>
+        } -->
 </template>
+
+<style scoped>
+.navbar {
+    padding-block: 2.188rem;
+    background-color: var(--clr-white);
+}
+
+.navbar-links {
+    text-transform: uppercase;
+    letter-spacing: 2px;
+}
+
+.navbar-links > *:hover {
+    text-decoration: underline;
+}
+
+/* mobile */
+@media (max-width:639px) {
+    .navbar {
+        position: relative;
+        z-index: 1002;
+    }
+
+    .navbar-links {
+        position: absolute;
+        flex-direction: column;
+        width: 100%;
+        background-color: var(--clr-black);
+        color: var(--clr-white);
+        font-size: 1.5rem;
+        left: 0;
+        margin-top: 2.188rem;
+        padding: 3rem 1.5rem;
+    }
+
+    .open {
+        display: flex;
+    }
+
+    .close {
+        display: none;
+    }
+
+    .navbar-overlay {
+        position: fixed;
+        inset: 0;
+        background: var(--clr-black);
+        opacity: 0.6;
+        z-index: 1001;
+    }
+}
+
+/* tablet */
+@media (min-width:640px) {
+    .navbar {
+        padding-block: 4rem;
+    }
+
+    .navbar-links {
+        display: flex;
+        gap: 2.625rem;
+        font-size: .875rem;
+    }
+
+    .mobile-menu {
+        display: none;
+    }
+}
+</style>
