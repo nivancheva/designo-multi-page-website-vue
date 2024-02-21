daa67d2e9360ad8cbb3458c99c8dab8e30218620
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-02-21 16:03:39 +0200

    add contact page

---
+++ src/components/Form.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>This is Form</div>
+</template>
+
+<style scoped>
+input,
+textarea {
+    width: 100%;
+    background-color: inherit;
+    color: var(--clr-white);
+    outline: 0;
+
+    border-bottom: 1px solid var(--clr-white-opacity);
+    padding-inline: 1rem;
+    padding-bottom: .688rem;
+
+    cursor: pointer;
+}
+
+input::placeholder,
+textarea::placeholder {
+    color: var(--clr-white);
+    opacity: .6;
+}
+
+input:focus,
+textarea:focus {
+    border-bottom-color: var(--clr-white);
+}
+
+.message-input {
+    padding-bottom: 3rem;
+}
+
+.errors {
+    align-items: center;
+    gap: .5rem;
+    font-size: .75rem;
+    font-style: italic;
+
+    position: absolute;
+    right: 0;
+    bottom: .5rem;
+}
+/* mobile */
+@media (max-width:639px) {
+    .form-button {
+        justify-self: center;
+    }
+}
+
+/* tablet */
+@media (min-width:640px) {
+    .form-button {
+        justify-self: end;
+    }
+}
+
+/* desktop */
+@media (min-width:864px) {
+   
+}
+</style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,3 +1,78 @@
+<script setup>
+import Countries from '../components/Countries/Countries.vue'
+import Form from '../components/Form.vue'
+
+</script>
+
 <template>
-    <h2>Contact</h2>
+    <div className="">
+            <div className="contact-form">
+                <div className="contact-form_text">
+                    <h1>Contact Us</h1>
+                    <p>Ready to take it to the next level? Let&apos;s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that&apos;s relatable to your users, drop us a line.</p>
+                </div>
+                <Form />
+            </div>
+            <Countries />
+            <div className="section-gap"></div>
+        </div>
 </template>
+
+<style scoped>
+.contact-form {
+    margin-inline: auto;
+    padding: 4.5rem 1.5rem;
+    display: grid;
+    gap: 3rem;
+
+    background-color: var(--clr-peach);
+    color: var(--clr-white);
+
+    background-image: url('../../../public/contact/mobile/bg-pattern-hero-contact-mobile.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.contact-form h1 {
+    margin-bottom: 1.5rem;
+}
+
+/* mobile */
+@media (max-width:639px) {
+    .contact-form {
+        text-align: center;
+    }
+}
+
+/* tablet */
+@media (min-width:640px) {
+    .contact-form {
+        max-width: 680px;
+        width: 90%;
+        padding: 4.438rem 3.625rem;
+        gap: 2.5rem;
+
+        border-radius: var(--border-radius);
+
+        background-image: url('../../../public/contact/desktop/bg-pattern-hero-desktop.svg');
+        background-position: top left;
+    }
+
+    .contact-form h1 {
+        margin-bottom: 2rem;
+    }
+    
+}
+
+/* desktop */
+@media (min-width:864px) {
+    .contact-form {
+        max-width: 1111px;
+        gap: 5.938rem;
+        grid-template-columns: repeat(2, 1fr);
+        align-items: center;
+        padding: 3.438rem 6rem;
+    }
+
+}
+</style>
