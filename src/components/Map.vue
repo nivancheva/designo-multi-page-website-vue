f8a6ccaf130ae7e8fe495e9307009ee9ed41309c
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-02-22 12:55:12 +0200

    add leaflet map

---
+++ src/components/Map.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  position: Array
+})
+
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet"
+</script>
+
+<template>
+  <l-map ref="map" :zoom="13" :center="position" :useGlobalLeaflet="false">
+    <l-tile-layer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        layer-type="base"
+        name="Stadia Maps Basemap">
+    </l-tile-layer>
+    <l-marker :lat-lng="position" ></l-marker>
+</l-map>
+</template>
+
+<style scoped>
+
+</style>
