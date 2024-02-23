34472664cc162bf9e4fe3e952df3da3773c996f1
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-02-23 13:01:04 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/designo-multi-page-website-vue/",
+  base: "/",
   plugins: [
     vue(),
   ],
